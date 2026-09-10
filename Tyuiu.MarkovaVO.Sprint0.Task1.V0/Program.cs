20794f4aa1b27826aeb05f0c83edb8039c8043b9
Var-ky <m.varya2008@bk.ru>
2026-09-10 16:41:13 +0500

    Add project files.

---
+++ Tyuiu.MarkovaVO.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.MarkovaVO.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Варвара";
+            int ago = 18;
+            Console.WriteLine("Здравствуйте, меня зовут" + name + ", мне" + ago + "лет");
+            Console.ReadKey();
+
+        }
+    }
+}
