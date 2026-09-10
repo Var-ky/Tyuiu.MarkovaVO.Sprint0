9dd8ca4c1a94cd5a23d127ab1c78d4ab64956c9f
Var-ky <m.varya2008@bk.ru>
2026-09-10 18:15:35 +0500

    Task2

---
+++ Tyuiu.MarkovaVO.Sprint).Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.MarkovaVO.Sprint0.Task2.V0.lib;
+namespace Tyuiu.MarkovaVO.Sprint_.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Варвара"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tyuiu.MarkovaVO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MarkovaVO.Sprint0.Task2.V0.lib;
+
+namespace Tyuiu.MarkovaVO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekGetMessageValid()
+        {
+            var name = "Варвара";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Варвара", res);
+        }
+    }
+}
